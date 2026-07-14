2bd19c32c5980c26324bbbc0cbd5af263a818507
Bassil Edelbi <bassilede88@gmail.com>
2026-07-14 19:10:53 +0200

    Refactor: add configuration barrel exports

---
+++ config/index.ts
@@ -0,0 +1,14 @@
+import businessConfig from "./business.config";
+
+export { businessConfig };
+export type {
+  BusinessAddress,
+  ContactInformation,
+  SocialLinks,
+  BrandAssets,
+  SEODefaults,
+  CompanyInformation,
+  LegalInformation,
+  OrganizationSchema,
+  SiteConfig,
+} from "./business.config";
